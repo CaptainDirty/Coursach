e8733f050ea580456bc634f4f7cd39607619350c
Bolotov.Alexander@urfu.me <Bolotov.Alexander@urfu.me>
2021-01-27 14:28:49 +0500

    Исправил мат.библиотеку

--- Models/Formulas.cs
+++ Models/Formulas.cs
@@ -4,18 +4,30 @@ namespace MathLibrary
 {
     public class Formulas
     {
-        public Formulas()
-        {
-            parameters = TemperatureDependenceOfParameters.GetParameters(tB);
-        }
 
         public double W { get; set; }
-        public double tB { get; set; }
+        private double _tB;
+        public double tB
+        {
+            get
+            {
+                return _tB;
+            }
+            set
+            {
+                _tB = value;
+                parameters = TemperatureDependenceOfParameters.GetParameters(tB);
+            }
+        }
         public double d { get; set; }
 
         Parameters parameters;
 
-        public double Re() => Math.Round(W * d / parameters.V * Math.Pow(10, 6), 2);
+        public double Re()
+        {
+
+            return Math.Round(W * d / parameters.V * Math.Pow(10, 6), 2);
+        }
 
         public double NuKor
         {
@@ -27,7 +39,10 @@ namespace MathLibrary
             get => Math.Round(0.37 * Math.Pow(Re(), 0.6), 2);
         }
 
-        public double a3r(double Nu) => Math.Round((Nu * parameters.Lambda * Math.Pow(10, -2) / d), 2);
+        public double a3r(double Nu)
+        {
+            return Math.Round((Nu * parameters.Lambda * Math.Pow(10, -2) / d), 2);
+        }
 
     }
 }
